304cbaacd2f6f412c563b05f9301e1ae721ce1a7
angrystar170 <angrystar170@gmail.com>
2025-04-07 22:42:15 +0900

    Update boot.js

--- packages/backend/src/server/web/boot.js
+++ packages/backend/src/server/web/boot.js
@@ -216,15 +216,15 @@
 		errorsElement.appendChild(detailsElement);
 		addStyle(`
 		* {
-		  	  @font-face {
-		  	      font-family: 'ONEMobilePOP';
-		  	      src: url('https://misskey.njy17.kr/files/59eb8a17-25a9-4d85-afc0-62857d2e03fd') format('woff');
-		  	      font-weight: normal;
-		  	      font-style: normal;
-	    	  }
+		    @font-face {
+		       font-family: 'ONEMobilePOP';
+		       src: url('https://misskey.njy17.kr/files/59eb8a17-25a9-4d85-afc0-62857d2e03fd') format('woff');
+		       font-weight: normal;
+		       font-style: normal;
+	    	}
 
 	        font-family: 'ONEMobilePOP', BIZ UDGothic, Roboto, HelveticaNeue, Arial, sans-serif;
-	  	}
+	  }
 
 
 		#misskey_app,
